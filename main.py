8ad506c78eb2dbe1b3e584df4ad9f7b5c702a09c
ArturSierakowski <artur.sierakovski@gmail.com>
2023-03-14 16:39:20 +0100

    laby 2

--- main.py
+++ main.py
@@ -1,3 +1,124 @@
-import sys
+# a = "napis"
+# print(a)
+# print(type(a))
+# b = "aaaa"
+# print(a+b)
+#
+# b = 5
+# print(a+str(b))
+#
+# c = 8
+# d = 3
+# print (c, d)
+#
+# e = c+d
+# print(e)
+#
+# f = 2+3j
+# print(f)
+# print(type(f))
+#
+# print(c-d)
+# print(c//d)
+# print(c % d)
+# print(c**d)
+# print(2**(1/2))
+# print(c+2)
+#
+# c += 2
+# print(c)
 
-print(sys.version)
+#listy
+
+listy = [2, 5, 6, 7, 6.5]
+# print(listy)
+#
+# listy.append(4)
+# print("wstawienie elementu na koniec", listy)
+#
+# listy.insert(4, 2)
+# print("wstawienie elementu na indeks", listy)
+#
+# listy.remove(4)
+# print("usuniecie elementu o wartosci", listy)
+#
+# listy.pop(2)
+# print("usuniecie elementu z pozycji 2", listy)
+#
+# listy.sort()
+# print("sortowanie listy", listy)
+#
+# listy.extend([1, 2, 3])
+# print("dodanie sekwencji", listy)
+#
+# listy.reverse()
+# print("odwrocenie listy", listy)
+
+#dictionary
+
+# slownik = {1: 'a', 2: 'b', 3: 'c'}
+# print(slownik)
+# print()
+
+#wprowadzanie zmiennych
+
+# napis = input('wprowadz liczbe: ')
+# print(napis)
+# print(type(napis))
+# napis = int(napis)
+# print(type(napis))
+
+#instrukcje warunkowe
+
+# a = input('podaj a: ')
+# b = input('podaj b: ')
+# a = int(a)
+# b = int(b)
+#
+# if a > b:
+#     print("a wieksze od b")
+# elif a == b:
+#     print("a jest rowne b")
+# else:
+#     print("a jest mniejsze od b")
+
+#petle
+
+# for i in range(5):
+#     print(i)
+# else:
+#     print('koniec petli')
+#
+# for i in listy:
+#     print(i)
+#
+# for i in range(len(listy)):
+#     print(listy[i])
+
+# i = 0
+# while i < len(listy):
+#     print(listy[i])
+#     i += 1
+
+liczby = [2, 5, 6, 2, 7, 3, 2, 4]
+# a = input('podaj a: ')
+# a = int(a)
+# i = 0
+# while i < len(liczby):
+#     if liczby[i] - a == 0:
+#         print(str(a) + ' - ' + str(liczby[i]) + ' = 0')
+#     else:
+#         print(i)
+#     i += 1
+
+for i in range(liczby.count(2)):
+    liczby.remove(2)
+print(liczby)
+
+i = 0
+while i < len(liczby):
+    if liczby[i] == 2:
+        liczby.pop(i)
+    else:
+        i += 1
+print(liczby)
---
+++ zadania_lab2/zadania.py
@@ -0,0 +1,70 @@
+# import math
+#
+# print(math.exp(10))
+# print(math.log(5 + pow(math.sin(8), 2))**(1/6))
+# print(math.floor(3.55))
+# print(math.ceil(4.80))
+
+# imie = "ARTUR"
+# nazwisko = "SIERAKOWSKI"
+# imie = imie.capitalize()
+# nazwisko = nazwisko.capitalize()
+# print(imie, nazwisko)
+
+# napis = "Na na, la la la la la na na na na na"
+# print(napis.count("la"))
+
+piwo = "tyskie"
+# print(piwo[1], piwo[-1])
+
+zdanie = "wypilem " + piwo + " na hita"
+# zdanie = zdanie.split()
+# print(zdanie)
+
+# x = str(21)
+# y = float(21)
+# z = hex(21)
+# print(x, y, z)
+
+# sporty = ["plywanie", "rower", "narty"]
+# sporty.reverse()
+# sporty.append("kosz")
+# sporty.append("siata")
+# print(sporty)
+
+# slownik_skrotow = {"np": "na przyklad", "tzw": "tak zwany"}
+# print(slownik_skrotow)
+
+# slownik_gier = {"CoD": "Call of Duty", "CS": "Counter Strike", "WoW": "World of Warcraft", "LoL": "Liga Legend"}
+# print(len(slownik_gier), slownik_gier)
+
+# zdanie = input("wprowadz zdanie: ")
+# print(zdanie.count('a'))
+
+# a = input("wprowadz a: ")
+# b = input("wprowadz b: ")
+# c = input("wprowadz c: ")
+# a = int(a)
+# b = int(b)
+# c = int(c)
+#
+# if a >= b and a >= c:
+#     print(a, "jest najwieksza")
+# elif b >= a and b >= c:
+#     print(b, "jest najwieksza")
+# elif c >= a and c >= b:
+#     print(c, "jest najwieksza")
+
+lista = [5, 3.5, 2, 4, 1.0, 1.2]
+for i in range(len(lista)):
+    print(lista[i] ** 2)
+
+i = 0
+liczby = []
+while i < 10:
+    n = input("wprowadz liczbe: ")
+    n = int(n)
+    if n % 2 == 0:
+        liczby.append(n)
+    i += 1
+print(liczby)
